ea7cff8c5eab64e03dcfdf4db6e768646e5ad22f
ballpwd <35981405+ballpwd@users.noreply.github.com>
2020-09-22 10:14:06 +0700

    test
    test

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,37 +1,3 @@
-<<<<<<< Updated upstream
-import React, { useEffect, Fragment } from 'react';
-import logo from '../../assets/logo.svg';
-import googlelogin from '../../assets/signin.svg';
-import { Container, Row, Col } from 'reactstrap';
-
-
-const Login = () => {
-    return (
-        <Fragment>
-            <div className='bg fullscreen'>
-                <Container >
-                    <Row className='justify-content-center align-items-center logo'>
-                        <img src={logo} alt='Interask' className='img-fluid '></img>
-                    </Row>
-
-                    <Row className='justify-content-center align-items-center button'>
-                        <a href="/api/auth/google"><img src={googlelogin} alt='Interask' className='img-fluid' style={{width:'340px'}} /></a>
-                    </Row>
-
-                    <Row className='justify-content-center align-items-end footer'>
-                        <p>Create your own room for <a href='/organizer'>FREE organization.com</a></p>
-                    </Row>
-
-                </Container>
-            </div>
-        </Fragment >
-        
-    );
-};
-
-
-export default Login;
-=======
 import React, { Fragment } from 'react';
 import logo from '../../assets/logo.svg';
 import googlelogin from '../../assets/signin.svg';
@@ -75,4 +41,3 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps)(Login);
->>>>>>> Stashed changes
